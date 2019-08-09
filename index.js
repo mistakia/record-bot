183efba2a386dd012f14bcb68a900432540c818d
mistakia <1823355+mistakia@users.noreply.github.com>
2019-08-09 19:11:57 -0400

    feat: pin contacts

--- index.js
+++ index.js
@@ -19,26 +19,38 @@ const dataDir = path.resolve(os.homedir(), './.record-bot')
 if (!fs.existsSync(dataDir)) { fs.mkdirSync(dataDir) }
 
 const configFile = path.resolve(dataDir, './config.json')
+const saveConfig = () => jsonfile.writeFileSync(configFile, defaultConfig, { spaces: 2 })
+const defaultConfig = {
+  about: {
+    name: 'Bot',
+    bio: 'A Record Bot',
+    location: 'IPFS'
+  },
+  importPaths: [],
+  scrapePaths: [],
+  completedImports: [],
+  contacts: {},
+  contactLimit: 10,
+  contactExpirationLimit: 10 //days
+}
+
 if (!fs.existsSync(configFile)) {
-  const defaultConfig = {
-    about: {
-      name: 'Bot',
-      bio: 'A feed of music from various websites',
-      location: 'World Wide Web'
-    },
-    importPaths: [],
-    scrapePaths: [],
-    completedImports: []
-  }
-  jsonfile.writeFileSync(configFile, defaultConfig, { spaces: 2 })
+  saveConfig()
 }
 
 const config = jsonfile.readFileSync(configFile)
 
-if (typeof config.completedImports === 'undefined') {
-  config.completedImports = []
-  jsonfile.writeFileSync(configFile, config, { spaces: 2 })
-}
+const defaultProperties = Object.keys(defaultConfig)
+const configProps = Object.keys(config)
+let configUpdated = false
+defaultProperties.forEach((prop) => {
+  if (configProps.indexOf(prop) === -1) {
+    config[prop] = defaultConfig[prop]
+    configUpdated = true
+  }
+})
+
+if (configUpdated) saveConfig()
 
 const opts = {
   orbitdb: {
@@ -49,6 +61,67 @@ const opts = {
   }
 }
 const record = new RecordNode(opts)
+
+const pinContact = async (logId) => {
+  logger.log(`Pinning log: ${logId}`)
+  await record.contacts.connect(logId)
+  config.contacts[logId] = new Date()
+  saveConfig()
+}
+
+record.on('redux', async ({ type, payload }) => {
+  switch(type) {
+    case 'CONTACT_PEER_JOINED':
+      if (config.contacts[payload.logId]) {
+        config.contacts[payload.logId] = new Date()
+        saveConfig()
+      }
+      break
+
+    case 'RECORD_PEER_LEFT':
+      if (config.contacts[payload.logId]) {
+        config.contacts[payload.logId] = new Date()
+        saveConfig()
+      }
+      break
+
+    case 'RECORD_PEER_JOINED':
+      // If contact exists, update last seen
+      if (config.contacts[payload.logId]) {
+        config.contacts[payload.logId] = new Date()
+        saveConfig()
+        return
+      }
+
+      // add if below limit or have stale contacts
+      const logIds = Object.keys(config.contacts)
+      if (logIds.length < config.contactLimit) {
+        await pinContact(payload.logId)
+      } else {
+        const now = moment()
+        let contactsRemoved = true
+        const lastSeenCutOff = now.substract(config.contactExpirationLimit, 'days')
+        for (let i=0; i < logIds.length; i++) {
+          const logId = logIds[i]
+          const lastSeen = config.contacts[logId]
+          if (lastSeen.isBefore(lastSeenCutOff)) {
+            logger.log(`Purging log: ${logId}`)
+            await record.contacts.disconnect(logId)
+            delete config.contacts[logId]
+            contactsRemoved = true
+          }
+        }
+
+        if (contactsRemoved) {
+          await pinContact(payload.logId)
+        }
+      }
+      break
+
+    default:
+      return
+  }
+})
 
 record.on('ready', async () => {
   try {
@@ -75,7 +148,7 @@ record.on('ready', async () => {
       logger.log(`Importing ${importPath}`)
       await record.tracks.addTracksFromFS(importPath)
       config.completedImports.push(importPath)
-      jsonfile.writeFileSync(configFile, config, { spaces: 2 })
+      saveConfig()
     }
   } catch (e) {
     error(e)
